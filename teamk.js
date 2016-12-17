f151207a11e1f94e0f5f8327eca114000ff618cf
evelyncarrera22 <ecarrera@cristoreyny.org>
2016-12-17 17:28:06 +0000

    added some images and changes code to add the bcak ground and some of the items

---
+++ teamk.js
@@ -0,0 +1,11 @@
+// var goal = 100
+// if (goal === 100) {
+//     alert("");
+// }
+
+$(function() {
+    $( "#fade" ).on('click', function() {
+        $( "#fade" ).fadeOut( "slow", function() {
+        });
+    });
+});
